32818267b4212bea65a137f60be4abaa054200d2
Thiago Luiz Krul <Thiago Luiz Krul>
2016-02-08 13:57:12 -0200

    Meu Primeiro commit

---
+++ src/main/java/br/com/krul/App.java
@@ -0,0 +1,13 @@
+package br.com.krul;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
